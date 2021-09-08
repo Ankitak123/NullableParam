d202bca1b6dc98dd84c11dd8d14478947ae7d510
Ankesh <Ankesh@LENOVO>
2021-09-08 11:29:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NullableParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nullable Parameter");
+            Console.WriteLine("--------------------------------------------");
+            NullableMethod();
+            NonNullableMethod();
+        }
+        public static void NullableMethod()
+        {
+
+
+            Nullable<int> n = null;
+            Console.WriteLine("ByDefault value of Nullable ");
+            Console.WriteLine(n.GetValueOrDefault());
+
+
+            int? n1 = null;
+            Console.WriteLine("value of Nullable variable n1");
+            Console.WriteLine(n1.GetValueOrDefault());
+        }
+        public static void NonNullableMethod()
+        { 
+            int? n1 = 10;
+            Console.WriteLine("Nullable value of n1");
+            Console.WriteLine(n1.GetValueOrDefault());
+
+            Nullable<int> n2 = 50;
+            Console.WriteLine("Nullable value of n1");
+            Console.WriteLine(n2.GetValueOrDefault());
+
+        }
+    }
+}
